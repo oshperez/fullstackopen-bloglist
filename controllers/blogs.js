d02d370e9560aade7b22d4e60d78160742a2c1f9
Osmani <osmanihernandezperez@gmail.com>
2021-07-03 10:31:22 -0400

    part4-c

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,24 +1,39 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
+// Get all blogs
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
+// Add a new blog
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
-  
-  if (blog.title === undefined || blog.url === undefined) {
-    return response.status(400).end();
-  } else if (blog.likes === undefined) {
-    blog.likes = 0;
+  const body = request.body;
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: "missing title and/or url" });
   }
 
-  const result = await blog.save();
-  response.status(201).json(result);
+  if (!body.likes) {
+    body.likes = 0;
+  }
+  const count = await User.countDocuments();
+  const random = Math.floor(Math.random() * count);
+  const user = await User.findOne().skip(random);
+
+  const newBlog = { ...body, user: user.id };
+
+  const blog = new Blog(newBlog);
+
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog.id);
+  await user.save();
+
+  response.status(201).json(savedBlog);
 });
 
+// Update a blog
 blogsRouter.put("/:id", async (request, response) => {
   const id = request.params.id;
   const { title, author, url, likes } = request.body;
@@ -28,6 +43,7 @@ blogsRouter.put("/:id", async (request, response) => {
   response.json(updatedBlog);
 });
 
+// Remove a blog
 blogsRouter.delete("/:id", async (request, response) => {
   const id = request.params.id;
 
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,7 +3,9 @@ const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
 const helper = require("./test_helper");
+const bcrypt = require("bcrypt");
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 beforeEach(async () => {
   await Blog.deleteMany({});
@@ -72,9 +74,11 @@ describe("when adding blogs", () => {
     expect(lastBlog.likes).toBe(0);
   });
 
-  test("if title and url are missing, server responds with 404 status", async () => {
+  test("if title and url are missing, server responds with 400 status", async () => {
     const incompleteBlog = {
+      title: "",
       author: "Miriam Gates",
+      url: "",
       likes: 20098,
     };
 
@@ -95,7 +99,7 @@ describe("when updating a blog", () => {
 
     const blogsAtEnd = await helper.blogsInDb();
     const updatedBlog = blogsAtEnd[0];
-    
+
     expect(updatedBlog.likes).toBe(1000000);
     expect(blogsAtEnd).toContainEqual(replacementBlog);
   });
@@ -116,6 +120,102 @@ describe("when deleting a blog", () => {
 
     expect(titles).not.toContain(blogToDelete.title);
   });
+});
+
+describe("when there is initially one user in the datebase", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+    const passwordHash = await bcrypt.hash("classified", 10);
+    const user = new User({ username: "root", passwordHash });
+    await user.save();
+  });
+
+  test("a new user can be successfully created", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "fmcbeen",
+      name: "Flavia Mackbeen",
+      password: "flabeen",
+    };
+
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((user) => user.username);
+    expect(usernames).toContain(newUser.username);
+  });
+
+  test("creating a new user fails if username is already taken", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "root",
+      name: "Superuser",
+      password: "anonymous",
+    };
+
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    expect(result.body.error).toContain("`username` to be unique");
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  });
+
+  test(`creating a new user fails if username and/or password are not 
+        provided`, async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const userWithMissingCredentials = {
+      username: "",
+      name: "Bad User",
+      password: "",
+    };
+
+    const result = await api
+      .post("/api/users")
+      .send(userWithMissingCredentials)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    expect(result.body.error).toContain("missing credential");
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  });
+});
+
+test(`creating a new user fails if username and/or password are not at 
+      least 3 characters long`, async () => {
+  const usersAtStart = await helper.usersInDb();
+
+  const userWithMissingCredentials = {
+    username: "az",
+    name: "Wrong User",
+    password: "r",
+  };
+
+  const result = await api
+    .post("/api/users")
+    .send(userWithMissingCredentials)
+    .expect(400)
+    .expect("Content-Type", /application\/json/);
+
+  expect(result.body.error).toContain("invalid credentials");
+
+  const usersAtEnd = await helper.usersInDb();
+  expect(usersAtEnd).toHaveLength(usersAtStart.length);
 });
 
 afterAll(() => {
