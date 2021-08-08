50be2f8728c30886df2b18673dad2bc685df6be3
Osmani <osmanihernandezperez@gmail.com>
2021-08-08 15:55:41 -0400

    a new route for adding comments was implemented as well as a model for storing those comments in the database

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,18 +2,21 @@ const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const Comment = require("../models/comment");
 const middleware = require("../utils/middleware");
 
 // Get all blogs
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  const blogs = await Blog.find({})
+    .populate("user", { username: 1, name: 1 })
+    .populate("comments");
   response.json(blogs);
 });
 
 // Add a new blog
 blogsRouter.post("/", middleware.userExtractor, async (request, response) => {
   const body = request.body;
-  
+
   const decodedToken = request.user;
   if (!body.title || !body.url) {
     return response.status(400).json({ error: "title or url missing" });
@@ -62,5 +65,20 @@ blogsRouter.delete(
     response.status(204).end();
   }
 );
+
+blogsRouter.post("/:id/comments", async (request, response) => {
+  const blogId = request.params.id;
+  const body = request.body;
+
+  const commentObj = { content: body.content, date: new Date() };
+  const comment = new Comment(commentObj);
+  const savedComment = await comment.save();
+
+  const blog = await Blog.findById(blogId);
+  blog.comments = blog.comments.concat(comment.id);
+  await blog.save();
+
+  response.status(201).json(savedComment);
+});
 
 module.exports = blogsRouter;
